07be52097a8e1802cce326bca7a724de7cf7e12b
High Voltage <highvoltage@deepearth.uk>
2017-01-15 04:30:22 +0000

    hopefully made settings more sane/functional

--- GridHealthDeficitGradiant/opts.lua
+++ GridHealthDeficitGradiant/opts.lua
@@ -14,12 +14,17 @@ module.defaultDB = {
         ignore_self = false,
         always_show = false,
         threshold_percentage = true,
+        threshold = 0.5,
+        threshold_percentage_value = 0.5,
+        threshold_absolute_value = 10000,
     }
 }
 
 
+local function validateThreshold,
+
+
 module._opts = {
-    color = false,
     ignoreSelf = {
         name = "Ignore Self",
         desc = "Ignore heals cast by you.",
@@ -76,29 +81,67 @@ module._opts = {
             color.r, color.g, color.b, color.a = r, g, b, a 
         end,
     },
-    threshold_abs = {
-        name = "Use Percentage Threshold",
-        desc = "Use  a percetnage for the threshold",
-        type = "toggle", width = "double",
-        order = 120,
-        get = function()
-            return module.db.profile.deficit_gradiant.threshold_percentage
-        end,
-        set = function(_, v)
-            module.db.profile.deficit_gradiant.threshold_percentage = v
-            module:UpdateAllUnits()
-        end,
-    },
     threshold = {
         name = "Threshold",
         desc = "The threshold at which to change the indicators color.",
-        type = "input",
-        order = 121,
-        get = function(t)
-            return module.db.profile.deficit_gradiant.threshold_health
-        end,
-        set = function(t, v)
-            module.db.profile.deficit_gradiant.threshold_health = tonumber(v)
-        end
+        type = "group",
+        args = {
+            is_percentage = {
+                name = "Use Percentage Threshold",
+                desc = "Use  a percetnage for the threshold",
+                type = "toggle",
+                width = "double",
+                order = 1,
+                get = function()
+                    return module.db.profile.deficit_gradiant.threshold_percentage
+                end,
+                set = function(_, v)
+                    module.db.profile.deficit_gradiant.threshold_percentage = v
+
+                    local newThreshold
+                    if v then
+                        newThreshold = module.db.profile.deficit_gradiant.threshold_percentage_value
+                    else
+                        newThreshold = module.db.profile.deficit_gradiant.threshold_absolute_value
+                    end
+
+                    module.db.profile.deficit_gradiant.threshold_health = newThreshold
+                end
+            }
+            abs = {
+                name = "Absolute Value",
+                type = "input",
+                disabled = function()
+                  return not module.db.profile.deficit_gradiant.threshold_percetange
+                end
+                order = 2,
+                pattern = "^%s%d+%s*",
+                get = function(t)
+                  return module.db.profile.deficit_gradiant.threshold_absolute_value
+                end,
+                set = function(t, v)
+                  local numericValue = tonumber(strmatch("^%s(%d+)%s*"))
+                  module.db.profile.deficit_gradiant.threshold_health = numericValue
+                  module.db.profile.deficit_gradiant.threshold_absolute_value = numericValue
+                end
+            },
+            percentage = {
+                name = "Percetage Value",
+                type = "range",
+                isPercent = true,
+                max = 100,
+                min = 0
+                disabled = function()
+                    return module.db.profile.deficit_gradiant.threshold_percetange
+                end
+                order = 4,
+                get = function(t)
+                    return module.db.profile.deficit_gradiant.threshold_percentage_value
+                end,
+                set = function(t, v)
+                    module.db.profile.deficit_gradiant.threshold_absolute_value = v
+                end
+            }
+        }
     }
 }
